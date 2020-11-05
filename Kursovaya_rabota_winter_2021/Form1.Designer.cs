d730fc2e30f9ed4548847870dba1aa977afca7fe
Igor Sharangiya <igor_korenovsk@mail.ru>
2020-11-05 18:52:35 +0300

    init commit

---
+++ Kursovaya_rabota_winter_2021/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Kursovaya_rabota_winter_2021
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(785, 318);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.Form1_DragOver);
+            this.DragLeave += new System.EventHandler(this.Form1_DragLeave);
+            this.GiveFeedback += new System.Windows.Forms.GiveFeedbackEventHandler(this.Form1_GiveFeedback);
+            this.QueryContinueDrag += new System.Windows.Forms.QueryContinueDragEventHandler(this.Form1_QueryContinueDrag);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Kursovaya_rabota_winter_2021/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kursovaya_rabota_winter_2021
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+           
+            string[] files = (string[])(e.Data.GetData(DataFormats.FileDrop, false));
+            foreach (string file in files)
+            {
+                PictureEditor picture = new PictureEditor(file);
+                picture.DrawPicture(this.Controls);
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;        
+        }
+
+        private void Form1_DragLeave(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_DragOver(object sender, DragEventArgs e)
+        {
+          
+        }
+
+        private void Form1_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+        {
+           
+        }
+
+        private void Form1_QueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+        {
+           
+        }
+    }
+
+    class PictureEditor
+    {
+        PictureBox picture;
+        Button buttondelete;
+        Control.ControlCollection controlForm;
+
+        public PictureEditor(string filename)
+        {
+            picture = new PictureBox();
+            buttondelete = new Button();
+            buttondelete.Text = "Delete";
+            buttondelete.Click += new EventHandler(button_Click);
+            picture.ImageLocation = System.IO.Path.GetFullPath(filename);
+            picture.Width = 100;
+            picture.Height = 100;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            controlForm.Remove(picture);
+            controlForm.Remove(buttondelete);
+        }
+
+        public void DrawPicture(Control.ControlCollection Controls)
+        {
+            controlForm = Controls;
+            Controls.Add(picture);
+            Controls.Add(buttondelete);
+            picture.Location = new System.Drawing.Point(0, 0);
+            buttondelete.Location = new System.Drawing.Point(0, 100);
+        }
+    }
+}
