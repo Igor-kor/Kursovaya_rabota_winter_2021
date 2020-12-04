dd53ac89502eb4abede80c65cb2d28075678585d
Igor Sharangiya <igor_korenovsk@mail.ru>
2020-12-04 23:50:16 +0300

    сделал перетаскивание и сохранение в нужном порядке(есть баг с гифками их надо как-то обрабатывать)

---
+++ Kursovaya_rabota_winter_2021/PictureEditor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Kursovaya_rabota_winter_2021
+{
+    class PictureEditor
+    {
+        Control.ControlCollection Controls;
+        List<PictureEditorItem> pictures;
+
+        public PictureEditor(FlowLayoutPanel flowLayoutPanel)
+        {
+            Controls = flowLayoutPanel.Controls;
+            pictures = new List<PictureEditorItem>();
+        }
+        public void AddItem(string file)
+        {
+            PictureEditorItem picture = new PictureEditorItem(file);
+            pictures.Add(picture);
+            picture.DrawPicture(this.Controls);
+        }
+
+        public void GeneratePicture()
+        {
+            ImageMagick.MagickNET.Initialize();
+            var image = new ImageMagick.MagickImageCollection();
+
+            SortedDictionary<int, PictureEditorItem> saved = new SortedDictionary<int, PictureEditorItem>();
+
+            foreach (PictureEditorItem picture in pictures)
+            {
+                if (Controls.Find(picture.box.Name, false).Length > 0)
+                {
+                    saved.Add(Controls.IndexOf(Controls.Find(picture.box.Name, false)[0]), picture);                  
+                }
+            }
+
+            foreach (var img in saved.Values)
+            {
+                image.Add(img.picture.ImageLocation);
+                image[image.Count - 1].AnimationDelay = Convert.ToInt32(img.delay.Text);
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = "*.gif";
+            saveFileDialog.Filter = "Gif files(*.gif)|*.gif|All files(*.*)|*.*";
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.Title = "Сохранить gif изображение";
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel) return;
+            if (saveFileDialog.FileName.Length == 0) return;
+            image.Write(saveFileDialog.FileName);
+        }
+    }
+}
---
+++ Kursovaya_rabota_winter_2021/PictureEditorItem.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Kursovaya_rabota_winter_2021
+{
+    class PictureEditorItem 
+    {
+        public PictureBox picture;
+        Button buttondelete;
+        public TextBox delay;
+        Control.ControlCollection controlForm;
+        public GroupBox box;
+        bool moved = false;
+
+        public PictureEditorItem(string filename)
+        {
+            picture = new PictureBox();
+            buttondelete = new Button();
+            delay = new TextBox();
+            box = new GroupBox();
+            box.Name = filename;
+            delay.Text = "100";
+            buttondelete.Text = "Delete";
+            picture.Click += new EventHandler(picture_DoubleClick);
+            buttondelete.Click += new EventHandler(button_Click);
+            picture.ImageLocation = System.IO.Path.GetFullPath(filename);
+            picture.Width = 200;
+            picture.Height = 200;
+            box.Width = 200;
+            box.Height = 280;
+            picture.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Remove();
+        }
+
+        public void Remove()
+        {
+            controlForm.Remove(box);
+        }
+
+        private void picture_DoubleClick(object sender, EventArgs e)
+        {
+            FullPicture fullPicture = new FullPicture();
+            PictureBox picturefull = new PictureBox();
+            picturefull.Image = picture.Image;
+            picturefull.Width = picture.Image.Width;
+            picturefull.Height = picture.Image.Height;
+            fullPicture.Controls.Add(picturefull);
+            fullPicture.Visible = true;
+            fullPicture.AutoScroll = true;
+            fullPicture.Width = picture.Image.Width;
+            fullPicture.Height = picture.Image.Height;
+        }
+
+        public void DrawPicture(Control.ControlCollection Controlscol)
+        {
+            controlForm = Controlscol;
+            box.Controls.Add(picture);
+            box.Controls.Add(buttondelete);
+            box.Controls.Add(delay);
+            box.AllowDrop = true;
+            picture.MouseDown += new MouseEventHandler(BoxMouseDown);
+            picture.MouseUp += new MouseEventHandler(BoxMouseUP);
+            picture.MouseMove += new MouseEventHandler(BoxMouseMove);
+            Controlscol.Add(box);
+            picture.Location = new System.Drawing.Point(0,0);
+            buttondelete.Location = new System.Drawing.Point(0,200);
+            delay.Location = new System.Drawing.Point(0,230);           
+        }
+        
+        private void BoxMouseDown(object sender, MouseEventArgs e)
+        {
+            moved = true;
+            Cursor.Current = Cursors.SizeWE;
+        } 
+
+        private void BoxMouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+        private void BoxMouseUP(object sender, MouseEventArgs e)
+        {
+            if (moved)
+            {
+                
+                if (e.X > 0 && controlForm.IndexOf(box) < (controlForm.Count-1))
+                {                   
+                    controlForm.SetChildIndex(box, (e.X / box.Width) + controlForm.IndexOf(box));
+                }
+                if(e.X < 0 && controlForm.IndexOf(box) > 0)
+                {
+                    controlForm.SetChildIndex(box, (e.X / box.Width - 1) + controlForm.IndexOf(box));
+                }
+              
+               moved = false;
+            }
+            Cursor.Current = Cursors.Default;
+        }
+    }
+}
